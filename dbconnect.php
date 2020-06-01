8c67f64bcbe874c287413080cce7176584a96d7a
ak-akshay <kak.akshay17@gmail.com>
2020-06-01 13:10:43 +0530

    Removed Credentials

--- dbconnect.php
+++ dbconnect.php
@@ -5,8 +5,8 @@ try {
     $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
     // -------------Manager for online database-------------------
-    // !!!!!!!!------Set <password> before use----------!!!!!!!!!
-    // $manager = new MongoDB\Driver\Manager('mongodb://userakshay:<password>@pinngcluster-shard-00-00-sdbuq.mongodb.net:27017,pinngcluster-shard-00-01-sdbuq.mongodb.net:27017,pinngcluster-shard-00-02-sdbuq.mongodb.net:27017/test?ssl=true&replicaSet=PinngCluster-shard-0&authSource=admin&retryWrites=true&w=majority');
+    // !!!!!!!!------Set <username> and <password> before use----------!!!!!!!!!
+    // $manager = new MongoDB\Driver\Manager('mongodb://<username>:<password>@pinngcluster-shard-00-00-sdbuq.mongodb.net:27017,pinngcluster-shard-00-01-sdbuq.mongodb.net:27017,pinngcluster-shard-00-02-sdbuq.mongodb.net:27017/test?ssl=true&replicaSet=PinngCluster-shard-0&authSource=admin&retryWrites=true&w=majority');
 }
 catch(MongoDB\Driver\Exception\Exception $e) {
     echo "Exception:", $e->getMessage(), "\n";
